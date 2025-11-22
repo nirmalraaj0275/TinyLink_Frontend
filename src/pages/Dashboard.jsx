5fc032e821e38b5227d9cd192800c6ffcba52ce9
nirmalraaj0275 <124169316+nirmalraaj0275@users.noreply.github.com>
2025-11-22 14:03:20 +0530

    commit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -25,7 +25,7 @@ export default function Dashboard() {
 
     const checkShortCode = async () => {
       try {
-        const res = await fetch(`http://localhost:5000/api/links/${value}`);
+        const res = await fetch(`${import.meta.env.VITE_API_URL}/api/links/${value}`);
 
         if (res.status === 404) {
           setNotFound(true);
