04025efaa9c3ad28711da202da14266f700720f7
nirmalraaj0275 <124169316+nirmalraaj0275@users.noreply.github.com>
2025-11-22 14:07:10 +0530

    commit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -67,7 +67,7 @@ export default function Dashboard() {
   ----------------------------------------------------- */
   const loadLinks = async () => {
     try {
-      const res = await fetch("http://localhost:5000/api/links");
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/links/${data}`);
       const data = await res.json();
       setLinks(data);
     } catch {
